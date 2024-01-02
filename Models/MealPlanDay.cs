725c6c471178f79c780c7dd6a4cac312d921e110
Tobias J <tobias.jaks@gmail.com>
2024-01-02 14:58:35 +0100

    can now create a meal plan in cache mem.

---
+++ Models/MealPlanDay.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileCookbook.Models
+{
+    public class MealPlanDay
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [Ignore]
+        public DateTime Date { get; set; }
+
+        public string DateSerialized
+        {
+            get => JsonConvert.SerializeObject(Date);
+            set => Date = string.IsNullOrEmpty(value) ? default : JsonConvert.DeserializeObject<DateTime>(value);
+        }
+
+        [Ignore]
+        public List<OneMeal> Meals { get; set; }
+
+        public string MealsSerialized
+        {
+            get => JsonConvert.SerializeObject(Meals);
+            set => Meals = JsonConvert.DeserializeObject<List<OneMeal>>(value ?? string.Empty) ?? new List<OneMeal>();
+        }
+    }
+}
---
+++ Models/OneMeal.cs
@@ -0,0 +1,16 @@
+﻿using MauiCookbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileCookbook.Models
+{
+    public class OneMeal
+    {
+        public string MealName { get; set; }
+        public RecipeTypes MealType { get; set; }
+        public int Portions { get; set; }
+    }
+}
